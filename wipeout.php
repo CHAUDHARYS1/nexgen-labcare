275b2e33f1b61656cf91b2f706a30b5d3b57c6f2
Shital Chaudhary <CHAUDHARYS1@users.noreply.github.com>
2021-07-01 14:16:51 -0400

    add wipe out product page

---
+++ wipeout.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<link rel="icon" href="img/dark-favicon.png" type="image/gif" sizes="16x16">
+
+	<!-- Stylesheet -->
+	<link rel="stylesheet" href="css/styles.css">
+
+	<!-- Font Stylesheet -->
+	<link rel="stylesheet" href="https://use.typekit.net/nck5uye.css">
+	<title>Nexgen Labcare</title>
+
+</head>
+
+<body>
+	<?php include('fragments/nav.php'); ?>
+
+
+
+	<div class="container-fluid" style="background: #2F4D84;" id="wipeoutSectionOne">
+		<div class="container" id="attentionGrabberContainer">
+			<div class="row">
+				<div class="col-lg-4 col-md-0" id="modelBackpack">
+					<img src="img/packet-bag.png" style="min-height: 900px;" alt="">
+				</div>
+				<div class="col-md-4">
+					<div class="attention-grabber">
+						<h1 class="display-4" style="">Our Wipeout Hand Sanitizer. <span style="font-weight: 700;color:white;background: black;line-height:1.5;">Confidence You Can Carry.</span></h1>
+						<br>
+						<p style="" class="text-white">Wipeout Hand Sanitizer is the perfect pocket size packet you never leave home without. </p>
+
+						<p class="font-weight-bold text-white mt-4" style="font-size: 20px;">Easy to use and easy to carry.</p>
+						<a href="#amazon" class="btn bg-white mt-4 buy-now" style="font-weight: 700"><img src="img/icons-shopping_cart.png" alt="">Buy Now</a>
+					</div>
+				</div>
+				<div class="col-md-4 order-first order-lg-last ">
+					<img src="img/og-wipeout-packet-xl.png" class="attention-grabber-img" alt="">
+				</div>
+
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="mt-5 pt-5">
+			<h2 class="font-weight-bold">Why Wipeout?</h2>
+		</div>
+		<div class="row" id="wipeoutSectionTwo">
+			<div class="col-md-4" id="backPocket"></div>
+			<div class="col-md-8 mt-5 pl-5 convenient-to-carry">
+				<h1>Convenient to Carry</h1>
+				<h3 style="margin-top: 50px;line-height: 1.5">Wipeout Hand Sanitizer is a<strong> one of a
+						kind</strong>, convenient way to carry hand sanitizer
+					gel when you <strong>leave the house, go to work,
+						buy the groceries, or go for a walk</strong>
+					around the neighborhood.</h3>
+			</div>
+		</div>
+
+
+
+		<div id="wipeoutSectionThree">
+			<img src="img/packet-plant.jpg" width="100%" alt="">
+			<div>
+				<h1 class="display-2 mt-5">Contains Aloe,
+					Vitamin E, and
+					Tea Tree Oil</h1>
+				<h3 class="mt-5">Powerful ingredients - <b>Aloe, Vitamin E, and Tea Tree
+						Oil </b>- provide beneficial cleansing and moisturizing
+					power to leave your hands feeling <b>clean and
+						refreshed.</b></h3>
+			</div>
+		</div>
+
+		<div class="row" id="wipeoutSectionFour">
+			<div class="col-lg-4" id="packetSize"></div>
+			<div class="col-lg-8 mt-5 pt-5">
+				<h1 class="display-2">Perfect Size for your Pocket</h1>
+				<br>
+				<h3 class="">Wipeout Gel Packets are ideal for many occasions where carrying a bottle is not practical.</h3>
+				<h3 class="mt-4">
+					Each packet is 4.5 inches tall by 2.5 inches in width. Making it the <b>perfect size to carry in your hand.</b></h3>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid bg-light" id="wipeoutSectionFive">
+		<div class="container">
+			<h1>Simple as 1 & 2</h1>
+			<div class="row mt-5 no-gutters">
+				<div class="col-md-6 text-right">
+					<img src="img/step1.jpg" alt="" class="step-1">
+					<div class="text-center">
+						<p>1. Open</p>
+					</div>
+				</div>
+				<div class="col-md-6 text-left">
+
+					<img src="img/step2.jpg" alt="" class="step-2">
+					<div class="text-center">
+						<p>2. Squeeze</p>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="container" id="wipeoutSectionSix">
+		<h1>Made in the US</h1>
+		<br>
+		<h3>Our products are formulated
+			and manufactured in world
+			class FDA approved labs in
+			the United States.</h3><br>
+		<h3>
+			Made in the USA is a sign of
+			<b>confidence and high quality.</b></h3><br>
+		<h3>
+			Nexgen Labcare is a company
+			you can depend on for
+			<b>excellence and quality.</b></h3>
+	</div>
+
+
+	<?php include('fragments/instagram-footer.php');?>
+	<?php include('fragments/footer.php');?>
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
