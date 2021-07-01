ef462c3d4ac6eebd84cc35663adc162477934c1d
Shital Chaudhary <CHAUDHARYS1@users.noreply.github.com>
2021-07-01 14:16:21 -0400

    add separate footer section - social footer/normal footer

---
+++ fragments/footer.php
@@ -0,0 +1,32 @@
+	<footer class="container-fluid" style="background: #132038">
+
+		<div class="footer-links">
+			<ul class="nav justify-content-center">
+				<li class="nav-item">
+					<a class="nav-link link text-muted p-2" href="/">Our Story</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link link text-muted p-2" href="/products">Our Products</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link link text-muted p-2" href="/privateLabel">Private Label</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link link text-muted p-2" href="/contact">Contact Us</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link link text-muted p-2" href="http://www.shopify.com">Buy Now</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link link text-muted p-2" href="#">Privacy Policy</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link link text-muted p-2" href="#">Terms of Service</a>
+				</li>
+
+			</ul>
+		</div>
+		<div class="text-muted all-rights-reserved">
+			<p>&copy 2020 Nexgen Labcare LLC. All right reserved.</p>
+		</div>
+	</footer>
---
+++ fragments/social-footer.php
@@ -0,0 +1,7 @@
+<div class="container-fluid text-white pt-3 pb-3" style="background: #24375A;">
+	<div class="container text-center">
+		<a href="" target="_blank"><img src="img/icons8-facebook.png" height="50" alt=""></a>
+		<a href="" target="_blank" class="ml-3"><img src="img/icons8-instagram_new.png" height="50" alt=""></a>
+		<a href="" target="_blank" class="ml-3"><img src="img/icons8-twitter.png" height="50" alt=""></a>
+	</div>
+</div>
