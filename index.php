adad000b680940122a866c73a04d7ec28b36017a
Shital Chaudhary <CHAUDHARYS1@users.noreply.github.com>
2021-06-30 20:33:39 -0400

    structure index page with proper html

---
+++ index.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<link rel="icon" href="img/dark-favicon.png" type="image/gif" sizes="16x16">
+
+	<!-- Stylesheet -->
+	<link rel="stylesheet" href="css/styles.css">
+
+	<!-- Font Stylesheet -->
+	<link rel="stylesheet" href="https://use.typekit.net/nck5uye.css">
+	<title>Nexgen Labcare</title>
+
+	<!-- Animation CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css" />
+</head>
+
+<body>
+	<!-- Nav Begins Here -->
+	<?php include('fragments/nav.php'); ?>
+	<!-- End -->
+	<!-- HOME PAGE CONTENT | L E F T    &    R I G H T    S I D E  -->
+	<div class="wrapper row no-gutters">
+		<div class="col-lg-8">
+			<div id="leftSide">
+				<div class="container wow animate__animated animate__fadeInLeft animate__delay-3s" id="leftSideContent">
+					<!-- Headline -->
+					<h1 class="font-weight-bold" style="font-size:56px!important;">A New Approach to Hand Sanitizer.</h1>
+
+					<!-- SubHeadline -->
+					<p>To keep ourselves and our loved ones safe, quick-access single-use hand sanitizer is essential.</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-4 text-center text-white" id="wipeoutPacketContent">
+			<div class="packet-showcase">
+				<!-- Packet Image -->
+				<img src="img/og-wipeout-packet-xl.png" class="og-packet  wow animate__animated animate__zoomInDown animate__delay-2s" alt="Original Wipeout Packet">
+
+				<!-- Featured Product -->
+				<p class="wow animate__animated animate__fadeInUp">Featured Product</p>
+
+				<!-- Product Name -->
+				<h3 class="font-weight-bold wow animate__animated animate__fadeInUp animate__delay-1s">Wipeout Hand <br> Sanitizer</h3>
+
+				<!-- Link to Learn More -->
+				<a href="#" class="link learn-more wow animate__animated animate__fadeInUp animate__delay-3s">Learn more <img src="img/link-arrow.png" alt="Arrow Icon"></a>
+
+				<!-- Spacing -->
+				<br>
+
+				<!-- Purchase Button -->
+				<a href="http://www.amazon.com" class="btn btn-lg btn-scale shadow mt-4" style="min-width:50%;"><img src="img/whit-cart.png" alt="Icon of a cart" class="mr-2">Buy Now</a>
+			</div>
+		</div>
+	</div>
+
+	<!-- ************************  END  ************************ -->
+
+	<!-- HOME PAGE CONTENT |  T W I T T E R    S H O W C A S E -->
+	<div class="container-fluid bg-grey pb-5">
+		<div class="container" id="secondContainer">
+			<div class="row">
+				<div class="col-md-5">
+					<div class="sticky-top">
+						<!-- Twitter Embed -->
+						<blockquote class="twitter-tweet">
+							<p lang="en" dir="ltr">Vote once<br>Wash your hands repeatedly <br>Do both like your life- and the lives of your loved ones and fellow citizens- depend on it.<br>Because they do <a href="https://t.co/7EmtmlUNN9">https://t.co/7EmtmlUNN9</a></p>&mdash; Arne Duncan (@arneduncan) <a href="https://twitter.com/arneduncan/status/1285618437532852224?ref_src=twsrc%5Etfw">July 21, 2020</a>
+						</blockquote>
+
+						<!-- Required JavaScript File for Twitter Embed -->
+						<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+					</div>
+				</div>
+				<div class="offset-1"></div>
+
+				<div class="col-md-6" id="secondRightSide">
+					<div class="sticky-top">
+						<!-- Spacing -->
+						<br>
+
+						<!-- Headline -->
+						<h2>Why you should use our products</h2><br>
+
+						<!-- Three Paragraphs -->
+						<p>Introducing: Wipeout Hand Sanitizer, confidence you can carry. It’s enough for a single -use, but also enough to share — because keeping others safe keeps you safe too.</p>
+
+						<p class="arne-duncan"><span id="here">Here’s</span> what former Obama Education Secretary, Arne Duncan had to say about our latest client work.</p>
+
+						<p>Why buy our products? Affordable, convenient, and built-with-empathy in mind — not only is our single-use product among the only shareable products on the market, all of our products are blended, packed, and distributed here in the U.S., by American companies … because the only way we recover, is together.</p>
+
+
+						<!-- Spacing -->
+						<br>
+
+						<!-- Link -->
+						<a href="#" class="link">Check out more about our private labeling option<img src="img/link-arrow.png" alt="Arrow Icon"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Social Media Footer -->
+	<?php include('fragments/social-footer.php');?>
+	<!-- End -->
+	<!-- Website Footer -->
+	<?php include('fragments/footer.php');?>
+	<!-- End -->
+
+
+	<!-- WOW.JS Scroll to reveal animation -->
+	<script src="js/wow-library/wow.js"></script>
+	<script src="js/wow-library/wow.min.js"></script>
+	<!-- Add JavaScript Below If Needed -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
