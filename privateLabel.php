186d8af99af37c7b03b081c7b78fd8e181746fdc
Shital Chaudhary <CHAUDHARYS1@users.noreply.github.com>
2021-07-01 14:18:12 -0400

    add private labeling page

---
+++ privateLabel.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html xmlns:th="http://www.thymeleaf.org">
+
+<head>
+
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+	<!-- Favicon -->
+	<link rel="icon" href="img/dark-favicon.png" type="image/gif" sizes="16x16">
+
+	<!-- Stylesheet -->
+	<link rel="stylesheet" href="css/styles.css">
+
+	<!-- Font Stylesheet -->
+	<link rel="stylesheet" href="https://use.typekit.net/nck5uye.css">
+	<link href="https://fonts.googleapis.com/css2?family=Special+Elite&display=swap" rel="stylesheet">
+	<!-- IMPOTANT | TWO FONTS REQUIRED | SECOND FONT ONLY USED FOR BLOCKQUOTE | USE SECOND FONT ONLY WHEN NECESSARY  -->
+	<title>Private Label | Nexgen Labcare</title>
+
+</head>
+
+<body>
+	<!-- Nav Begins Here -->
+	<?php include('fragments/nav.php'); ?>
+	<!-- End -->
+	<!-- PRIVATE LABEL | F I R S T   S E C T I O N -->
+	<div class="wrapper row">
+		<div id="privateLabel" class="col-lg-8">
+			<div id="privateLabelContent">
+				<!-- Main Headline -->
+				<h1>Private Labeling</h1>
+				<p>Slapping a label on a bottle of hand sanitizer is easy. Our approach is different. We are an extension of your needs — from the first idea to holding the packet in your (or your customer’s) hand.</p>
+				<p>We know you don’t have the money and time to spin up your own division to handle our new-normal, which is why we’re here to help. We have experience with corporations and law firms big and small, and also with grassroots, door-to-door campaigns (where in-person interaction is essential).</p>
+				<p class="p-two">There’s no one else like us in the market. Don’t take our word for it:here’s our latest work:</p>
+			</div>
+		</div>
+		<!-- Background Image -->
+		<div class="col-lg-4" id="privateLabelMainImg"></div>
+	</div>
+	<!-- PRIVATE LABEL | S E C O N D    S E C T I O N -->
+	<div class="second-wrapper row">
+		<div class="col-lg-6" id="privateLabelSecondImg">
+			<!-- Complex Quote Section -->
+			<div class="blockquote-container">
+				<blockquote>"Having in person conversations to increase voter turnout is what we do. Nexgen Labcare’s individual hand sanitizer packet was a critical tool that allowed us to fulfill our mission in the COVID environment." </blockquote>
+				<cite style="line-height:1;" class="text-center">
+					<p>Harry Pascal, Founder/CEO</p>
+					<p class="font-weight-bold">Progressive Turnout Project</p>
+				</cite>
+			</div>
+		</div>
+		<div class="col-lg-6 bg-grey" id="privateLabelSecondContainer">
+			<div id="privateLabelSecondContent">
+				<!-- Headline -->
+				<h2>Voting Matters</h2>
+				<p class="p-two font-weight-bold pr-2">Together with our client, we built a nationwide distribution
+					program using hand sanitizer as the medium and the message.</p>
+				<p class="p-two pr-4">Our packets are part of a 15+ state effort to encourage folks to exercise their right to vote.Packets are printed with a trademarked phrase - representing the organization’s goal and mission-critical purpose.</p>
+				<!-- Spacing -->
+				<br><br>
+				<!-- Tags -->
+				<div class="row tags-container">
+					<div class="col-sm">
+						<h2 class="text-light-blue">15+</h2>
+						<h4 class="font-weight-bold">States</h4>
+					</div>
+
+
+					<div class="col-sm">
+						<h2 class="text-light-blue">750k</h2>
+						<h4 class="font-weight-bold">Packets</h4>
+					</div>
+					<div class="offset-1"></div>
+					<div class="col-sm">
+						<h2 class="text-light-blue">3wk</h2>
+						<h4 class=" font-weight-bold">LeadTime</h4>
+					</div>
+					<div class="offset-1"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- PRIVATE LABEL | T H I R D   S E C T I O N -->
+	<div class="container-fluid">
+		<div class="container" id="privateLabelProcess">
+			<!-- Headline -->
+			<div class="mt-5 text-center">
+				<h2>Private Labeling</h2>
+				<p>If you ever thought of having your own brand, here is your chance. This is how it’s done…</p>
+			</div>
+			<!-- Cards -->
+			<div class="row no-gutters" id="privateLabelProcessContent">
+				<div class="col-md-3">
+					<div class="private-label-cards" id="designCard">
+						<div class="private-label-cards-content">
+							<img src="img/design-icon.jpg" alt="Private Label Design Icon">
+							<br><br><br>
+							<h4 class="font-weight-bold">1. Design</h4> <br>
+							<p>Collaborate with our creative team to perfect design</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-1 big-arrows" id="bigArrowOne">
+					<div class="private-label-arrow-icons">
+						<img src="img/icons8-chevron_right.png" height="70" alt="">
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="private-label-cards" id="approvalCard">
+						<div class="private-label-cards-content">
+							<img src="img/approval-icon.jpg" alt="Private Label Approval Icon">
+							<br><br><br>
+							<h4 class="font-weight-bold">2. Approval</h4> <br>
+							<p>Collaborate with our creative team to perfect design</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-1 big-arrows" id="bigArrowTwo">
+					<div class="private-label-arrow-icons">
+						<img src="img/icons8-chevron_right.png" height="70" alt="">
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="private-label-cards" id="productionCard">
+						<div class="private-label-cards-content">
+							<img src="img/icons8-services.png" alt="Private Label Production Icon">
+							<br><br><br>
+							<h4 class="font-weight-bold">3. Production</h4> <br>
+							<p>Collaborate with our creative team to perfect design</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Dramatic -->
+			<div id="dramaticSection" class="text-center">
+				<h3 class="">Interested?</h3>
+				<a href="http://www.amazon.com" class="btn btn-lg shadow btn-scale mt-4">Yes, Get Started!</a>
+
+			</div>
+		</div>
+	</div>
+	<!-- END -->
+	<!-- Social Media Footer -->
+	<?php include('fragments/social-footer.php');?>
+	<!-- End -->
+	<!-- Website Footer -->
+	<?php include('fragments/footer.php');?>
+	<!-- End -->
+	<!-- Add JavaScript Below If Needed -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
