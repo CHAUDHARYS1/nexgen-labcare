776b857b21b7dfb4f68fcacf2541cad52ca3bff3
Shital Chaudhary <CHAUDHARYS1@users.noreply.github.com>
2021-07-01 14:13:52 -0400

    separate navigation page

---
+++ fragments/nav.php
@@ -0,0 +1,54 @@
+<!-- Mobile Navigation -->
+<div class="collapse navbar-collapsetext-right" id="mobileNavbar" class="responsive-nav">
+	<ul class="navbar-nav">
+		<!-- <li class="nav-item">
+			<a class="nav-link text-white" href="about.php">Our Story</a>
+		</li> -->
+		<li class="nav-item">
+			<a class="nav-link text-white" href="ourProducts.php">Our Products</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link text-white" href="privateLabel.php">Private Label</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link text-white" href="contact.php">Contact US</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link text-white" href="">Buy Now</a>
+		</li>
+	</ul>
+</div>
+<!-- End -->
+<!-- ADD 'sticky-top' to the line below to experiment  -->
+
+<!-- Web Navigation -->
+<nav class="navbar navbar-expand-lg bg-white">
+	<a class="navbar-brand" href="#"><img src="img/logo.png" class="logo" alt="Nexgen Labcare Logo"></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobileNavbar" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+		<img src="img/icon-menu-01.png" height="30" alt="">
+	</button>
+
+	<div class="navbar-collapse justify-content-end" id="webNavbar" class="web-nav">
+		<ul class="navbar-nav mt-2 mt-lg-0 text-center">
+
+			<!-- <li class="nav-item">
+				<a class="nav-link" href="about.php">Our Story</a>
+			</li> -->
+			<li class="nav-item">
+				<a class="nav-link" href="ourProducts.php">Our Products</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="privateLabel.php">Private Label</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="contact.php">Contact Us</a>
+			</li>
+			<!-- Main Btn -->
+			<li class="nav-item" id="navbarMainBtn">
+				<a class="btn shadow nav-link disabled" href="">Buy Now</a>
+			</li>
+		</ul>
+
+	</div>
+</nav>
+<!-- End -->
