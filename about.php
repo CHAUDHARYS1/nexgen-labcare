d6e1e4c42ca5435cb84992e50f653524516f3722
Shital Chaudhary <CHAUDHARYS1@users.noreply.github.com>
2021-06-30 20:37:46 -0400

    add about page

---
+++ about.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<link rel="icon" href="img/dark-favicon.png" type="image/gif" sizes="16x16">
+
+	<!-- Stylesheet -->
+	<link rel="stylesheet" href="css/styles.css">
+
+	<!-- Font Stylesheet -->
+	<link rel="stylesheet" href="https://use.typekit.net/nck5uye.css">
+	<title>Nexgen Labcare</title>
+
+
+</head>
+
+<body>
+	<?php include('fragments/nav.php'); ?>
+	<div id="getToKnowUs">
+		<div class="text-white text-center" style="padding-top: 35vh;">
+			<h1 class="display-1 font-weight-bold">Get to Know us</h1>
+			<ul class="nav justify-content-center">
+				<li class="nav-item mr-3">
+					<a class="nav-link text-white" href="#ourStory"><u>Our Story</u></a>
+				</li>
+				<li class="nav-item  mr-3">
+					<a class="nav-link text-white" href="#ourMission"><u>Our Mission</u></a>
+				</li>
+				<li class="nav-item  mr-3">
+					<a class="nav-link text-white" href="#ourCoreValues"><u>Core Values</u></a>
+				</li>
+				<li class="nav-item  mr-3">
+					<a class="nav-link text-white" href="#support"><u>Support</u></a>
+				</li>
+				<li class="nav-item  mr-3">
+					<a class="nav-link text-white" href="#feedback"><u>Connect with us</u></a>
+				</li>
+			</ul>
+		</div>
+	</div>
+
+	<div id="whyYouWillLoveUs" class="container">
+		<div class="row mt-5 pt-5">
+			<div class="col-lg-4" id="nexgenBuilding">
+			</div>
+			<div class="col-lg-7 offset-1 mt-5 pt-5" id="ourStory">
+				<p class="subtext">Nexgen Labcare</p>
+				<h1>Why you’ll love us</h1><br> <br>
+				<p class="p"><strong>Nexgen Labcare sells high quality hygiene products to businesses and consumers.</strong> Leveraging over 30+ years in formulation,
+					manufacturing, and fulfillment, our team is world class and based in the United States.
+				</p><br>
+				<p class="p">
+					Private label services will help you make a product your own. From design to formulation, production and delivery,<strong> we help
+						turn your vision in to reality.</strong>
+				</p>
+			</div>
+		</div>
+
+	</div>
+
+	<div id="ourMission" class="">
+		<div class="container" id="ourMissionContent">
+			<p class="subtext">Why we do what we do</p>
+			<h1>Our Mission</h1>
+			<p style="max-width: 700px;margin: 0 auto;">We are a company that follows what any other company would practice but what makes us different is what we choose to focus on to satisfy our customers.</p><br><br>
+			<p>We focus on bringing people with new ways to stay clean. </p>
+		</div>
+
+	</div>
+
+	<div id="ourCoreValues">
+		<div class="container" style="margin-top: 100px;">
+
+			<div>
+				<p class="subtext">What we believe in</p>
+				<h1>Our Core Values</h1>
+			</div>
+			<div class="row no-gutters mt-5">
+				<div class="col-md-4 mb-2" id="coreValueOne">
+					<div class="core-value-trait">Honesty</div>
+				</div>
+				<div class="col-md-4 mb-2" id="coreValueTwo">
+					<div class="core-value-trait">Excellence</div>
+				</div>
+				<div class="col-md-4 mb-2" id="coreValueThree">
+					<div class="core-value-trait">Team work</div>
+				</div>
+			</div>
+			<p class="p text-center" style="max-width: 800px;margin: 0 auto;">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+
+		</div>
+	</div>
+
+	<div class="container-fluid blue-rectangle" id="support"> </div>
+	<div class="container-fluid online-cusomter-support">
+		<div class="container">
+			<div class="row no-gutters">
+				<div class="col-lg-5">
+					<p class="subtext">We answer your questions</p>
+					<h1>Online Customer Support</h1>
+					<br>
+					<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit…</p>
+					<button class="mt-4">Customer Support</button>
+				</div>
+				<div class="col-lg-7">
+					<img src="img/customer-support/21502.png" class="mt-5" style="width: 100%;" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid text-white" style="background: #304D83;padding: 50px;" id="feedback">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6">
+					<h3>Email Us</h3>
+					<p>We love you and you and you…</p>
+				</div>
+				<div class="col-sm-6 text-right">
+					<button>Feedback</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+	<?php include('fragments/instagram-footer.php');?>
+	<?php include('fragments/footer.php');?>
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
