526ced32d32940d21f95818388a357afa307c0e1
Shital Chaudhary <CHAUDHARYS1@users.noreply.github.com>
2021-07-01 14:15:35 -0400

    add thank you page after contact page is submitted

---
+++ thankyou.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<link rel="icon" href="img/dark-favicon.png" type="image/gif" sizes="16x16">
+
+	<!-- Stylesheet -->
+	<link rel="stylesheet" href="css/styles.css">
+
+	<!-- Font Stylesheet -->
+	<link rel="stylesheet" href="https://use.typekit.net/nck5uye.css">
+	<title>Nexgen Labcare</title>
+
+	<!-- Animation CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css" />
+</head>
+<style>
+	#thankYouContainer {
+		background: #507ED5;
+	}
+
+	#thankYouContent {
+		margin-top: 50%;
+		color: white;
+		text-align: center;
+	}
+
+	#touchWithUsContainer {
+		background: #2F4D84;
+	}
+
+	#touchWithUsContent {
+		margin-top: 35%;
+		text-align: center;
+		color: white;
+	}
+
+</style>
+
+<body>
+
+	<div class="container-fluid">
+		<div class="row" style="min-height:100vh;">
+			<div class="col-lg-5" id="thankYouContainer">
+				<div id="thankYouContent">
+					<h1>Thank you!</h1>
+				</div>
+			</div>
+			<div class="col-lg-7" id="touchWithUsContainer">
+				<div id="touchWithUsContent">
+					<h3>Thank you for getting in touch with us.</h3>
+					<h3>We will get back to you as soon as we can. </h3>
+					<h5 class="mt-5 pt-5">Usually in 24/hrs.</h5>
+					<a href="#" class="link"><img src="img/left-arrow.png" alt="Arrow Icon" class="mr-2">Go Back</a>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
